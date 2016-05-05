62ac671b4ccea34d8283fc80a8c2b7d032602611
carlquan <carlquan@CarlQuan.Beijing.Objectiva.local>
2016-05-05 11:08:28 +0800

    hasAuthority replace hasRole

--- src/main/java/org/networking/config/SecurityConfig.java
+++ src/main/java/org/networking/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package org.networking.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,51 +18,38 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * Created by Gino on 8/28/2015.
  */
 @Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
-    private static final String USERS_BY_USERNAME_QUERY =
-            "select username as principal, password as credentials, true from USER where username = ?";
+	private static final String USERS_BY_USERNAME_QUERY = "select username as principal, password as credentials, true from USER where username = ?";
 
-    private static final String AUTHS_BY_USERNAME_QUERY =
-            "select u.username as principal, a.role as role " +
-                    "from AUTHORITY a join USER u ON u.id = a.user_id where u.username = ?";
+	private static final String AUTHS_BY_USERNAME_QUERY = "select u.username as principal, a.role as role "
+			+ "from AUTHORITY a join USER u ON u.id = a.user_id where u.username = ?";
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(USERS_BY_USERNAME_QUERY).passwordEncoder(encoder)
-                .authoritiesByUsernameQuery(AUTHS_BY_USERNAME_QUERY);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(USERS_BY_USERNAME_QUERY)
+				.passwordEncoder(encoder).authoritiesByUsernameQuery(AUTHS_BY_USERNAME_QUERY);
+	}
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-            .ignoring()
-                .antMatchers("/resources/**");
-    }
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-        	.antMatchers("/","/products","/product-**","/infinite-business-solutions","/contact","/h2-console/**")
-        		.permitAll()
-    		//.antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
-            .permitAll()
-            .deleteCookies("remember-me")
-            	.and()
-            	.exceptionHandling().accessDeniedPage("/unauthorized");
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/", "/products", "/product-**", "/infinite-business-solutions", "/contact",
+						"/h2-console/**")
+				.permitAll()
+				// .antMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll()
+				.deleteCookies("remember-me").and().exceptionHandling().accessDeniedPage("/unauthorized");
+	}
 }
--- src/main/java/org/networking/config/SpringConfig.java
+++ src/main/java/org/networking/config/SpringConfig.java
@@ -1,9 +1,10 @@
 package org.networking.config;
 
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
 /**
  * Created by Gino on 9/22/2015.
@@ -11,9 +12,9 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public Jackson2ObjectMapperBuilder configureObjectMapper() {
-        return new Jackson2ObjectMapperBuilder().modulesToInstall(Hibernate4Module.class);
-    }
+	@Bean
+	public Jackson2ObjectMapperBuilder configureObjectMapper() {
+		return new Jackson2ObjectMapperBuilder().modulesToInstall(Hibernate4Module.class);
+	}
 
 }
