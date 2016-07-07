4a3b8d3926b1f63071bd435a6ff63cf83a683df9
carlquan <carlquan@CarlQuan.Beijing.Objectiva.local>
2016-07-07 09:18:12 +0800

    add oauth2 github

--- src/main/java/org/networking/Application.java
+++ src/main/java/org/networking/Application.java
@@ -1,24 +1,16 @@
 package org.networking;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Created by Gino on 8/28/2015.
  */
-@Configuration
-@EnableAutoConfiguration
-@EnableAuthorizationServer
-@EnableResourceServer
-@EnableOAuth2Client
+@SpringBootApplication
 @ComponentScan
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
--- src/main/java/org/networking/config/SecurityConfig.java
+++ src/main/java/org/networking/config/SecurityConfig.java
@@ -1,54 +1,218 @@
 package org.networking.config;
 
-import javax.sql.DataSource;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.CompositeFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
 
 /**
  * Created by Gino on 8/28/2015.
  */
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@EnableOAuth2Client
+@EnableAuthorizationServer
+@Order(6)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+//	@Autowired
+//	private DataSource dataSource;
+//	@Autowired
+//	OAuth2ClientContext oauth2ClientContext;
+	
+//	private static final String USERS_BY_USERNAME_QUERY = "select username as principal, password as credentials, true from USER where username = ?";
+//
+//	private static final String AUTHS_BY_USERNAME_QUERY = "select u.username as principal, a.role as role "
+//			+ "from AUTHORITY a join USER u ON u.id = a.user_id where u.username = ?";
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		PasswordEncoder encoder = new BCryptPasswordEncoder();
+//		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(USERS_BY_USERNAME_QUERY)
+//				.passwordEncoder(encoder).authoritiesByUsernameQuery(AUTHS_BY_USERNAME_QUERY);
+//	}
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		/*http.csrf().disable().authorizeRequests()
+//				.antMatchers("/products", "/product-**", "/infinite-business-solutions", "/contact",
+//						"/h2-console/**")
+//				.permitAll()
+//				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+//				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll()
+//				.deleteCookies("remember-me").and().exceptionHandling().accessDeniedPage("/unauthorized");*/
+//		http.antMatcher("/**")
+//		.authorizeRequests()
+//			.antMatchers("/", "/login**", "/webjars/**").permitAll()
+//			.anyRequest().authenticated()
+//		.and().exceptionHandling().accessDeniedPage("/unauthorized")
+//		.and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+//		.and().logout().logoutSuccessUrl("/").permitAll()
+//		.and().csrf().csrfTokenRepository(csrfTokenRepository())
+//		.and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+//		.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+//	}
+	
 	@Autowired
-	private DataSource dataSource;
+	OAuth2ClientContext oauth2ClientContext;
 
-	private static final String USERS_BY_USERNAME_QUERY = "select username as principal, password as credentials, true from USER where username = ?";
-
-	private static final String AUTHS_BY_USERNAME_QUERY = "select u.username as principal, a.role as role "
-			+ "from AUTHORITY a join USER u ON u.id = a.user_id where u.username = ?";
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(USERS_BY_USERNAME_QUERY)
-				.passwordEncoder(encoder).authoritiesByUsernameQuery(AUTHS_BY_USERNAME_QUERY);
-	}
-
-	@Override
-	public void init(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/","/webjars/**","/resources/**");
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-				.antMatchers("/products", "/product-**", "/infinite-business-solutions", "/contact",
-						"/h2-console/**")
-				.permitAll()
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll()
-				.deleteCookies("remember-me").and().exceptionHandling().accessDeniedPage("/unauthorized");
+		// @formatter:off	
+		http.antMatcher("/**")
+			.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+			.and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+			.and().logout().logoutSuccessUrl("/").permitAll()
+			.and().csrf().csrfTokenRepository(csrfTokenRepository())
+			.and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+			.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+		// @formatter:on
+	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration
+			extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.antMatcher("/me")
+				.authorizeRequests().anyRequest().authenticated();
+			// @formatter:on
+		}
+	}
+
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(
+			OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+	@Bean
+	@ConfigurationProperties("github")
+	ClientResources github() {
+		return new ClientResources();
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook")
+	ClientResources facebook() {
+		return new ClientResources();
+	}
+
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		filters.add(ssoFilter(facebook(), "/login/facebook"));
+		filters.add(ssoFilter(github(), "/login/github"));
+		filter.setFilters(filters);
+		return filter;
+	}
+
+	private Filter ssoFilter(ClientResources client, String path) {
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				path);
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(client.getClient(),
+				oauth2ClientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(new UserInfoTokenServices(
+				client.getResource().getUserInfoUri(), client.getClient().getClientId()));
+		return facebookFilter;
+	}
+
+	private Filter csrfHeaderFilter() {
+		return new OncePerRequestFilter() {
+			@Override
+			protected void doFilterInternal(HttpServletRequest request,
+					HttpServletResponse response, FilterChain filterChain)
+							throws ServletException, IOException {
+				CsrfToken csrf = (CsrfToken) request
+						.getAttribute(CsrfToken.class.getName());
+				if (csrf != null) {
+					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+					String token = csrf.getToken();
+					if (cookie == null
+							|| token != null && !token.equals(cookie.getValue())) {
+						cookie = new Cookie("XSRF-TOKEN", token);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+				filterChain.doFilter(request, response);
+			}
+		};
+	}
+
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+
+}
+
+class ClientResources {
+	private OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
+	private ResourceServerProperties resource = new ResourceServerProperties();
+
+	public OAuth2ProtectedResourceDetails getClient() {
+		return client;
+	}
+
+	public ResourceServerProperties getResource() {
+		return resource;
 	}
 }
