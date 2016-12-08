b69bc03d345ffe4d983fbd9517fb44150370edc7
carlquan <carlquan1986@icloud.com>
2016-12-08 16:47:14 +0800

    add dubbo-starter dependence  into spring-boot

--- src/main/java/org/networking/service/impl/DubboImplService.java
+++ src/main/java/org/networking/service/impl/DubboImplService.java
@@ -1,13 +1,13 @@
 package org.networking.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
-import org.networking.service.dubboInterface;
+import org.networking.service.DubboInterface;
 
 /**
  * Created by djy on 2016/12/5.
  */
 @Service(version = "1.0.0")
-public class dubboImplService implements dubboInterface {
+public class DubboImplService implements DubboInterface {
 
     @Override
     public String sayHello() {
